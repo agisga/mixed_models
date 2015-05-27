f070d9fcb0459e0f4d8801f3e58576bcf469833a
agisga <alexej.go@googlemail.com>
2015-05-27 11:44:00 -0500

    Fixed some small things

--- lib/MixedModels/pls.rb
+++ lib/MixedModels/pls.rb
@@ -5,15 +5,15 @@ require 'nmatrix'
 #
 class Deviance 
 
-  # Create linear mixed model pprofiled deviance function
-  # or the REML criterion the penalized least squares (PLS) approach.
+  # Create linear mixed model profiled deviance function
+  # or the REML criterion using the penalized least squares (PLS) approach.
   #
   # === Arguments
   #
   # * +x+ - fixed effects model matrix
   # * +y+ - response
-  # * +zt+ - transpose of the sparse model matrix for the random effects
-  # * +lambdat+ - upper triangular sparse Cholesky factor of the
+  # * +zt+ - transpose of the random effects model matrix
+  # * +lambdat+ - upper triangular Cholesky factor of the
   #               relative covariance matrix of the random effects.
   # * +thfun+ - a +Proc+ object that takes a value of +theta+ and produces
   #             the non-zero elements of +Lambdat+.  The structure of +Lambdat+
@@ -22,7 +22,7 @@ class Deviance
   # * +offset+ - offset
   # * +reml_flag+ - indicator whether to calculate REML criterion 
   #
-  def initialize(x:, y:, zt:, lambdat:, thfun:, weights: nil, offset: nil, reml_flag: true)
+  def initialize(x:, y:, zt:, lambdat:, thfun:, weights: nil, offset: 0.0, reml_flag: true)
     unless x.is_a?(NMatrix) and y.is_a?(NMatrix) and zt.is_a?(NMatrix) and lambdat.is_a?(NMatrix)
       raise ArgumentError, "x, y, zt, lambdat should be passed as NMatrix objects"
     end
@@ -30,24 +30,24 @@ class Deviance
     raise ArgumentError, "weights should be passed as an array" unless weights.is_a?(Array)
 
     @x, @y, @zt, @lambdat, @thfun, @weights, @offset, @reml_flag = x, y, zt, lambdat, thfun, weights, offset, reml_flag 
-    @n, @p, @q = @y.shape[0], @X.shape[1], @Zt.shape[0]
+    @n, @p, @q = @y.shape[0], @x.shape[1], @zt.shape[0]
  
-    unless @x.shape[0]==@n and @zt.shape[1]==@n and @lambdat.shape[0]==@q and 
-           @lambdat.shape[1]==@q and @weights.length==@n
+    unless @x.shape[0]==@n and @zt.shape[1]==@n and @lambdat.shape[0]==@q and @lambdat.shape[1]==@q
       raise ArgumentError, "Dimensions mismatch"
     end
 
     @sqrtw = if @weights.nil?
                NMatrix.identity(@n, dtype: :float64)
              else
+               raise ArgumentError, "weights should have the same length as the response vector y" unless @weights.length==@n
                NMatrix.diagonal(@weights, dtype: :float64)
              end
     
-    wx = sqrtw.dot x
-    wy = sqrtw.dot y
+    wx = @sqrtw.dot x
+    wy = @sqrtw.dot y
     @ztw = @zt.dot @sqrtw
     @xtwx = wx.transpose.dot wx
-    @xtwy = wx.transpose.dot Wy
+    @xtwy = wx.transpose.dot wy
     @ztwx = @ztw.dot wx
     @ztwy = @ztw.dot wy
     wx = nil
@@ -66,9 +66,16 @@ class Deviance
     @rzx = NMatrix.zeros([@q,@p], dtype: :float64) # intermediate matrix in solution
     @u = NMatrix.new([@q,1], dtype: :float64)      # conditional mode of spherical random effects
     @df = @n                                       # degrees of freedom (depends on REML)
-    @df = @df - @p if @reml_flag
+    @df -= @p if @reml_flag
   end
 
+  # Evaluate the linear mixed model profiled deviance function
+  # or the REML criterion at the value +theta+
+  #
+  # === Arguments
+  #
+  # * +theta+ - an array compatible with +thfun+ from Deviance#initialize
+  #  
   def eval(theta)
     # update the covariance factor +@lambdat@ and the Cholesky factor +@l+ 
     @lambdat = @thfun.call(theta)
@@ -79,7 +86,8 @@ class Deviance
     
     # solve the normal equations to get estimates +@beta+, +@u+ and +@b+
     @cu = @l.solve(@lambdat.dot @ztwy) 
-    @rzx = @l.solve(@lambdat.dot @ztwx)
+    #@rzx = @l.solve(@lambdat.dot @ztwx)
+    @rzx = @l.inverse.dot(@lambdat.dot @ztwx) #TODO: make a solve method for this
     @rxtrx = @xtwx - (@rzx.transpose.dot @rzx)
     @beta = @rxtrx.solve(@xtwy - (@rzx.transpose.dot @cu))
     @u = @l.transpose.solve(@cu - (@rzx.dot @beta))
@@ -89,13 +97,14 @@ class Deviance
     @mu = (@x.dot @beta) + (@zt.transpose.dot @b) + @offset
     @wtres = @sqrtw.dot (@y-@mu)
 
-    # evaluate the profiled deviance
-    # TODO: is NMatrix#norm2 more efficient here?
-    @pwrss <- (@wtres**2.0).sum + (@u**2).sum # penalized, weighted residual sum-of-squares
-    @logdet = 2.0 * Math::log(@l.abs.det) 
-    @logdet += Math::log(@rxtrx.abs.det) if @reml_flag
-                                # profiled deviance or REML
-                                # criterion (eqns. 34, 41)
+    # evaluate the profiled deviance or the REML criterion
+    #@pwrss = (@wtres**2.0).sum + (@u**2.0).sum # penalized, weighted residual sum-of-squares
+    @pwrss = (@wtres.norm2)**2.0 + (@u.norm2)**2.0 # penalized, weighted residual sum-of-squares
+    @logdet = 2.0 * Math::log(@l.det.abs) 
+    @logdet += Math::log(@rxtrx.det.abs) if @reml_flag
+    puts @df
+    puts @logdet
+    puts @pwrss
     deviance = @logdet + @df * (1.0 + Math::log(2.0 * Math::PI * @pwrss) - Math::log(@df))
     return deviance
   end
--- spec/matrix_methods_spec.rb
+++ spec/matrix_methods_spec.rb
@@ -1,25 +1,45 @@
 require 'MixedModels'
 
+ALL_DTYPES = [:byte,:int8,:int16,:int32,:int64, :float32,:float64, :object,
+  :rational32,:rational64,:rational128, :complex64, :complex128]
+
 describe NMatrix do
 
-  it "Compute the Kronecker product of two [1,n] NMatrix objects" do
-    a = NMatrix.new([1,3], [0,1,0])
-    b = NMatrix.new([1,2], [3,2])
-    m = a.kron_prod_1D b
-    expect(m).to eq(NMatrix.new([1,6],[0, 0, 3, 2, 0, 0]))
+  ALL_DTYPES.each do |dtype|
+    [:dense, :yale].each do |stype|
+      context "#kron_prod_1D #{dtype} #{stype}" do
+        before do 
+          a = NMatrix.new([1,3], [0,1,0], dtype: dtype, stype: stype)
+          b = NMatrix.new([1,2], [3,2], dtype: dtype, stype: stype)
+          m = a.kron_prod_1D b
+          it "Compute the Kronecker product of two [1,n] NMatrix objects" do
+            expect(m).to eq(NMatrix.new([1,6],[0, 0, 3, 2, 0, 0]), dtype: dtype, stype: stype)
+          end
+        end
+      end
+    end
   end
 
-  it "Generate a block-diagonal NMatrix" do
-    a = NMatrix.new([2,2],[1,2,3,4])
-    b = NMatrix.new([1,1],[123.0])
-    c = NMatrix.new([3,3],[1,2,3,1,2,3,1,2,3])
-    m = NMatrix.block_diagonal(a,b,c, dtype: :int32, stype: :yale)
-    expect(m).to eq(NMatrix.new([6,6], [1, 2,   0, 0, 0, 0,
-                                        3, 4,   0, 0, 0, 0,
-                                        0, 0, 123, 0, 0, 0,
-                                        0, 0,   0, 1, 2, 3,
-                                        0, 0,   0, 1, 2, 3,
-                                        0, 0,   0, 1, 2, 3], dtype: :int32, stype: :yale))
+  ALL_DTYPES.each do |dtype|
+    [:dense, :yale, :list].each do |stype|
+      context "#block_diagonal #{dtype} #{stype}" do
+        it "block_diagonal() creates a block-diagonal NMatrix" do
+          a = NMatrix.new([2,2],[1,2,
+                                 3,4])
+          b = NMatrix.new([1,1],[123.0])
+          c = NMatrix.new([3,3],[1,2,3,
+                                 1,2,3,
+                                 1,2,3])
+          m = NMatrix.block_diagonal(a,b,c, dtype: dtype, stype: stype)
+          expect(m).to eq(NMatrix.new([6,6], [1, 2,   0, 0, 0, 0,
+                                              3, 4,   0, 0, 0, 0,
+                                              0, 0, 123, 0, 0, 0,
+                                              0, 0,   0, 1, 2, 3,
+                                              0, 0,   0, 1, 2, 3,
+                                              0, 0,   0, 1, 2, 3], dtype: dtype, stype: stype))
+        end
+      end
+    end
   end
 
 end
