6a6088042a1f66ee0a7fba3429c6fe177b37aad9
agisga <alexej.go@googlemail.com>
2015-06-14 20:15:27 -0500

    Daru::Vector#to_indicator_cols_df and Daru::DataFrame#interaction_df_with (plus spec), which are auxiliary methods to create design matrices for categorical data

--- lib/MixedModels/daru_methods.rb
+++ lib/MixedModels/daru_methods.rb
@@ -16,6 +16,44 @@ module Daru
       0.upto(n-1) { |i| data_array.concat(self.row[i].to_a) }
       return NMatrix.new([n,m], data_array, dtype: dtype, stype: stype)
     end
+
+    # Create a data frame of interaction effects in the sense of linear models.
+    # That is, it returns a Daru::DataFrame containing all pair-wise products of
+    # vectors in +self+ and +other+
+    #
+    # === Arguments
+    #
+    # * +other+ - A Daru::DataFrame
+    #
+    # === Usage
+    # > df1 = Daru::DataFrame.new([[1,2],[3,4]], order: ['a','b'])
+    #          # => <Daru::DataFrame:69920382023980 @name = 900f7fbe-ad31-4ab3-8a16-232e22b17d53 @size = 2>
+    #                a          b 
+    #     0          1          3 
+    #     1          2          4 
+    #
+    # > df2 = Daru::DataFrame.new([[1,1],[2,2]], order: ['x','y'])
+    #           # => <Daru::DataFrame:69920381762120 @name = 3cb2690b-52a2-44b9-9a7e-686c8d92c38e @size = 2>
+    #                    x          y 
+    #         0          1          2 
+    #         1          1          2 
+    #
+    # > df1.interaction_df_with df2
+    #            # => <Daru::DataFrame:69920381579240 @name = d6773420-31ce-45dc-8639-b94b276b63a6 @size = 2>
+    #                   ax         ay         bx         by 
+    #         0          1          2          3          6 
+    #         1          2          4          4          8 
+    #
+    def interaction_df_with other
+      df = Daru::DataFrame.new([], order: [])
+      self.each_vector_with_index do |vec, name|
+        other.each_vector_with_index do |other_vec, other_name|
+          product_name = (name.to_s + other_name.to_s).to_sym
+          df[product_name] = vec * other_vec
+        end
+      end
+      return df
+    end
   end
 
   class Vector
@@ -29,6 +67,57 @@ module Daru
     def to_nm(dtype: :float64, stype: :dense)
       n = self.size
       return NMatrix.new([n,1], self.to_a, dtype: dtype, stype: stype)
+    end
+
+    # Auxiliary function which is useful for fitting of linear models.
+    # Transforms a Daru::Vector, whose entries are assumed to represent levels of
+    # a categorical variable, into a Daru::DataFrame with a column of zeros and ones
+    # for each category. If +full+ is set to false, then the first category is discarded,
+    # which is useful to generate a design matrix for linear regression, when a intercept term
+    # is present in the model.
+    #
+    # === Arguments
+    #
+    # * +name+ - used for the naming of the columns of the returned data frame
+    # * +for_model_without_intercept+ - if false (which is the default), then 
+    #                                   the first column of the produced data 
+    #                                   frame will be excluded
+    #
+    # === Usage
+    #
+    # a # => <Daru::Vector:70083983735480 @name = nil @size = 5 >
+    #           nil
+    #         0 1.0
+    #         1 2.0
+    #         2 3.0
+    #         3 1.0
+    #         4 1.0
+    #
+    # a.to_indicator_cols_df(name: 'MyVar', for_model_without_intercept: true) #   => 
+    #   #<Daru::DataFrame:70083988870200 @name = 08de5ef9-5c59-4acf-9853-04289d1a4ba5 @size = 5>
+    #                MyVar1.0   MyVar2.0   MyVar3.0 
+    #            0        1.0        0.0        0.0 
+    #            1        0.0        1.0        0.0 
+    #            2        0.0        0.0        1.0 
+    #            3        1.0        0.0        0.0 
+    #            4        1.0        0.0        0.0 
+    #
+    def to_indicator_cols_df(name:, for_model_without_intercept: false)
+      levels = self.to_a.uniq
+      names = levels.map { |e| name + e.to_s }
+      unless for_model_without_intercept 
+        levels.shift
+        names.shift
+      end
+
+      cols_array = Array.new
+      levels.each do |l|
+        col = Array.new
+        self.each { |e| e==l ? col.push(1.0) : col.push(0.0) }
+        cols_array.push(col)
+      end
+
+      return Daru::DataFrame.new(cols_array, order: names)
     end
   end
 end
---
+++ spec/daru_methods_spec.rb
@@ -0,0 +1,24 @@
+require 'MixedModels'
+
+describe Daru::DataFrame do
+  context "#interaction_df_with" do
+    it "creates a data frame of interaction effects in the sense of linear models" do
+      df1 = Daru::DataFrame.new([[1,2],[3,4]], order: ['a','b'])
+      df2 = Daru::DataFrame.new([[1,1],[2,2]], order: ['x','y'])
+      df  = Daru::DataFrame.new([[1,2],[2,4],[3,4],[6,8]], order: ['ax','ay','bx','by'])
+      expect(df1.interaction_df_with(df2).to_a).to eq(df.to_a)
+    end
+  end
+end
+
+describe Daru::Vector do
+  context "#to_indicator_cols_df" do
+    it "creates a data frame of indicator variables" do
+      a  = Daru::Vector.new([1,2,3,2,1,1])
+      df = Daru::DataFrame.new([[1.0,0.0,0.0,0.0,1.0,1.0],
+                                [0.0,1.0,0.0,1.0,0.0,0.0],
+                                [0.0,0.0,1.0,0.0,0.0,0.0]], order: ['v1','v2','v3'])
+      expect(a.to_indicator_cols_df(name: 'v', for_model_without_intercept: true).to_a).to eq(df.to_a)
+    end
+  end
+end
